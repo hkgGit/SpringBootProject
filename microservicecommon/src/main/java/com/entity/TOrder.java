5b0942be7b7a900491363f1fc111103bb4e9405a
hkgGit <1916657224@qq.com>
2019-12-27 13:43:37 +0800

    新建子公共模块

---
+++ microservicecommon/src/main/java/com/entity/TOrder.java
@@ -0,0 +1,15 @@
+package com.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TOrder {
+    private Long id; // 主键id
+    private String name; // 商品名称
+    private Double price; // 商品价格
+    private String dbSource; // 所存的数据库
+}
